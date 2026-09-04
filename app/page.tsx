ff9e11a7189bbde236a87096c95ca24d5172cb05
Hermitxxx <cvenom477@gmail.com>
2026-09-04 12:32:29 +0600

    refactoring the codebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { CinematicFooter } from "@/components/Footer";
+import HeroScroll from "@/components/HeroScroll";
 import { AnimeNavBar } from "@/components/Navbar";
 import ZoomInScroll from "@/components/ZoomInScroll";
 
 export default function HomePage() {
   return (
     <>
+      <HeroScroll></HeroScroll>
       <AnimeNavBar />
       <ZoomInScroll />
       <CinematicFooter />
